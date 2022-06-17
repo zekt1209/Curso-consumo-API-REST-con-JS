81addc8d290dc7e84586cc64170f72e0b810087f
Soporte Sistemas 3 <soporte.sistemas3@sto.com.mx>
2022-06-17 14:54:44 -0500

    Progreso - michis aleatorios

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
 
 const URL = 'https://api.thecatapi.com/v1/images/search';
 
+
 fetch(URL)
 .then(res => res.json())
 .then(data => {
     const img = document.querySelector('img');
     img.src = data[0].url; 
 });
+
+
+const getMichi = async () => {
+    const res = await fetch(URL);
+    const data = await res.json();
+    const img = document.querySelector('img');
+    img.src = data[0].url;
+}
+
+const myButton = document.querySelector("button");
+myButton.onclick = getMichi;
